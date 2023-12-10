4058b945f9c16a2c2ab6cd3673dda5fbd3d41240
David Kostoski <david.kostoski@gmail.com>
2023-12-10 20:41:33 +0100

    Starting to build a Job Board for Truck Drivers

---
+++ app/Models/Application.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Application extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'job_id',
+        'user_id',
+        'resume',
+        'cover_letter',
+        'application_date',
+        'status',
+    ];
+
+    public function job()
+    {
+        return $this->belongsTo(Job::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // ... other methods ...
+}
---
+++ app/Models/Job.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'company_name',
+        'location',
+        'description',
+        'requirements',
+        'skills',
+        'salary_range',
+        'application_deadline',
+        'status',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function applications()
+    {
+        return $this->hasMany(Application::class);
+    }
+
+    // Optional relationship for Company model
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    // ... other methods ...
+}
